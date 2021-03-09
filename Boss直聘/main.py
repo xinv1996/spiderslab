ad39b55e0e4a3dcf37a4cb462b531c3b15efd23f
xieruixin <xieruixin@qimingpian.com>
2021-03-09 13:39:29 +0800

    [feature] Boss直聘-爬虫

--- Boss直聘/main.py
+++ Boss直聘/main.py
@@ -1,28 +1,27 @@
-import random
 from urllib import parse
 from urllib.parse import quote
 
 import execjs
 import requests
 
-from ua_pool import chrome
 
 if __name__ == '__main__':
-    ua = random.choice(chrome)
+    ua = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 11_1_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.192 Safari/537.36'
     sess = requests.Session()
     proxies = {'http': 'http://127.0.0.1:1086', 'https': 'https://127.0.0.1:1086'}
     headers = {
         "user-agent": f"{ua}",
     }
-    url = "https://www.zhipin.com/job_detail/?query=%E5%8C%97%E4%BA%AC%E4%B9%BE%E5%9D%A4%E9%95%BF%E5%8D%BF%E7%A7%91%E6%8A%80%E6%9C%89%E9%99%90%E5%85%AC%E5%8F%B8"
-    response = requests.get(url, headers=headers, proxies=proxies)
+    url = f"https://www.zhipin.com/job_detail/?query={quote('百度')}&city=100010000&industry=&position="
+
+    response = sess.get(url=url, headers=headers)
     query_str = parse.urlparse(response.url).query
     query_dict = {i.split("=")[0]: i.split("=")[1] for i in query_str.split("&")}
     seed = parse.unquote(query_dict.get("seed"))
     ts = query_dict.get("ts")
     js_name = query_dict.get("name")
     js_url = f"https://www.zhipin.com/web/common/security-js/{js_name}.js"
-    js_res = sess.get(js_url, headers=headers, proxies=proxies)
+    js_res = sess.get(js_url,proxies=proxies)
     js_text = js_res.text
     js_text = js_text.replace("atob", "window.atob")
     js_code_format = '''
@@ -52,15 +51,25 @@ return encodeURIComponent(code)
     js = execjs.compile(js_code)
 
     zp_stoken = js.call('get_stoken', seed, ts)
+
     print(zp_stoken)
+
+    srcReferer = f"&srcReferer=https://www.zhipin.com/web/common/security-check.html?seed={quote(seed)}&name={js_name}&ts={ts}&callbackUrl={quote(url.replace('https://www.zhipin.com', ''))}&srcReferer="
     headers = {
+        "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "accept-encoding": "gzip, deflate, br",
+        "accept-language": "zh-CN,zh;q=0.9",
+        "sec-fetch-dest": "document",
+        "sec-fetch-mode": "navigate",
+        "sec-fetch-site": "same-origin",
+        "upgrade-insecure-requests": "1",
         "user-agent": f"{ua}",
         "cookie": f'__zp_stoken__={zp_stoken}',
         "referer": f"https://www.zhipin.com/web/common/security-check.html?seed={quote(seed)}&name={js_name}&ts={ts}&callbackUrl={quote(url.replace('https://www.zhipin.com', ''))}&srcReferer=",
     }
-    resp = sess.get(url=url, headers=headers,
-                        proxies=proxies)
-    resp.encoding = 'utf-8'
-    print(resp.text)
+    print(url)
+    resp = sess.get(url=url, headers=headers)
 
-    print(resp.status_code)
+    resp.encoding = 'utf-8'
+
+    print(resp.text)
